6085243425ea1c7af0d350097c1cb2954560ebd7
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-29 13:22:21 -0800

    added header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Footer() {
+  return (
+    <footer>
+      <div>&copy;2020. We own the libs.</div>
+    </footer>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Header() {
+  return (
+    <header>
+      <h1>Let&apos;s play Mad Libs</h1>
+    </header>
+  );
+}
