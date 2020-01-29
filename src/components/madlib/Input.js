bcd281783f8e52117f39320f03e8fd4302f19e26
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-29 15:49:13 -0800

    set value of input

--- src/components/madlib/Input.js
+++ src/components/madlib/Input.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ placeholder }) => (
-  <input type="text" placeholder={placeholder} />
+const Input = ({ placeholder, onChange, index, value }) => (
+  <input value={value} name={'input' + index} type="text" onChange={onChange} placeholder={placeholder} />
 );
 
 Input.propTypes = {
-  placeholder: PropTypes.string.isRequired
+  placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.string.isRequired
 };
 
 export default Input;
