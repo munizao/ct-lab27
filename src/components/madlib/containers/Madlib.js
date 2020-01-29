dcf80663ed43287a52278414e8083fb11f87d06b
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-29 15:47:42 -0800

    Added handleChange and handleClear

--- src/components/madlib/containers/Madlib.js
+++ src/components/madlib/containers/Madlib.js
@@ -4,23 +4,42 @@ import Result from '../Result';
 
 export default class Madlib extends Component{
   state = {
-    showResult: false
+    showResult: false,
+    words: new Array(12).fill("")
   }
 
   toggleResult = () =>
     this.setState(state => ({ ...state, showResult: !state.showResult }));
 
+
+  handleChange = ({ target }) => {
+    this.setState((state) => {
+      const newWords = state.words.slice();
+      const index = target.name.slice(5);
+      newWords[index] = target.value;
+      return { ...state, words:newWords };
+    });
+  };
+
   handleSubmit = event => {
     event.preventDefault();
+
     this.toggleResult();
+  }
+
+  handleClear = () => {
+    this.setState(() => ({
+      words: new Array(12).fill('')
+    }));
+    this.render();
   }
 
   render() {
     const { showResult } = this.state;
     return (
       <>
-        {!showResult && <Form onSubmit={this.handleSubmit} />}
-        {showResult && <Result words={[]} closeResult={this.toggleResult} />}
+        {!showResult && <Form words={this.state.words} onSubmit={this.handleSubmit} onChange={this.handleChange} onClear={this.handleClear} />}
+        {showResult && <Result words={this.state.words} closeResult={this.toggleResult} />}
       </>
     );
   }
