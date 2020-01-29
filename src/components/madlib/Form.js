d9a60ab79333a2137d610a3a124ef17f52e1b7db
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-29 15:48:52 -0800

    add clear button, map to make inputs

--- src/components/madlib/Form.js
+++ src/components/madlib/Form.js
@@ -2,26 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Input from './Input';
 
-const Form = ({ onSubmit }) => (
-  <form onSubmit={onSubmit}>
-    <Input placeholder="adjective" />
-    <Input placeholder="noun" />
-    <Input placeholder="verb, past tense" />
-    <Input placeholder="adverb" />
-    <Input placeholder="adjective" />
-    <Input placeholder="noun" />
-    <Input placeholder="noun" />
-    <Input placeholder="adjective" />
-    <Input placeholder="verb" />
-    <Input placeholder="adverb" />
-    <Input placeholder="verb, past tense" />
-    <Input placeholder="adjective" />
-    <button>Create Madlib</button>
-  </form>
+const partsOfSpeech = ['adjective', 'noun', 'verb, past tense',
+  'adverb', 'adjective', 'noun', 'noun',
+  'adjective', 'verb', 'adverb', 'verb, past tense', 'adjective'];
+
+const Form = ({ onSubmit, onChange, onClear, words }) => (
+  <div>
+    <form onSubmit={onSubmit}>
+      {partsOfSpeech.map((pos, i) => <Input value={words[i]} key={i} placeholder={pos} onChange={onChange} index={i} />)}
+      <button>Create Madlib</button>
+    </form>
+    <button onClick={onClear}>Clear Form</button>
+  </div>
 );
 
 Form.propTypes = {
-  onSubmit: PropTypes.func.isRequired
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onClear: PropTypes.func.isRequired,
+  words: PropTypes.array.isRequired
 };
 
 export default Form;
