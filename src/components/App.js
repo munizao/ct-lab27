2b19fd45f3eb2ee053faf30a0572f270ca83ac7a
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-29 17:37:30 -0800

    Dark mode

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Madlib from './madlib/containers/Madlib';
 import Header from './Header';
 import Footer from './Footer';
+import './App.css';
 
 export default function App() {
   return (
