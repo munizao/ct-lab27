7aa7d55d67ffd9b0ddf735a6317cb09efc073c08
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-29 16:22:39 -0800

    Style inputs

--- src/components/madlib/Input.js
+++ src/components/madlib/Input.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Input.css';
 
 const Input = ({ placeholder, onChange, index, value }) => (
-  <input value={value} name={'input' + index} type="text" onChange={onChange} placeholder={placeholder} />
+  <input className={styles.Input} value={value} name={'input' + index} type="text" onChange={onChange} placeholder={placeholder} />
 );
 
 Input.propTypes = {
